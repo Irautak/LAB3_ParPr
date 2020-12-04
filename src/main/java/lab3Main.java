d060b1bb20c66791118d4a5b808ace21c51873b8
Vlad Lomtev <vladlomtev@gmail.com>
2020-12-04 17:59:49 +0300

    Scripted auto-commit on change (2020-12-04 17:59:49) by gitwatch.sh

--- src/main/java/lab3Main.java
+++ src/main/java/lab3Main.java
@@ -1,2 +1,12 @@
+
+
+
+
 public class lab3Main {
+    public static void main(String[] args) {
+        SparkConf conf = new SparkConf().setAppName("lab3");
+        JavaSparkContent sc = new JavaSparkContext(conf);
+
+    }
+
 }
