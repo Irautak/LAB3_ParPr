8f752c854a7a8932c875c3a82a7f445cc02858fe
Vlad Lomtev <vladlomtev@gmail.com>
2020-12-04 20:10:35 +0300

    Scripted auto-commit on change (2020-12-04 20:10:35) by gitwatch.sh

--- src/main/java/lab3Main.java
+++ src/main/java/lab3Main.java
@@ -1,19 +1,25 @@
 import org.apache.spark.SparkConf;
 import org.apache.spark.api.java.JavaSparkContext;
+import org.apache.spark.broadcast.Broadcast;
 import scala.Tuple2;
 
 import java.util.Map;
+
+import static java.lang.Math.max;
 
 public class lab3Main {
     public static void main(String[] args) {
         SparkConf conf = new SparkConf().setAppName("lab3");
         JavaSparkContext sc = new JavaSparkContext(conf);
-        Map<Long, String> RDD = sc.textFile("file.csv")
+        final Broadcast<Map<Long, String>> airportsBroadcasted = sc.broadcast(
+                sc.textFile("file.csv")
                     .zipWithIndex()
                     .filter(lineWithInd -> lineWithInd._2() != 0)
                     .map(lineWithInd -> getAirportObj(lineWithInd._1()))
                     .mapToPair(airport -> new Tuple2<>(airport.getID(), airport.getName()))
-                    .collectAsMap();
+                    .collectAsMap());
+
+
         sc.textFile("sample.csv")
                 .zipWithIndex()
                 .filter(lineWithInd -> lineWithInd._2() != 0)
@@ -26,7 +32,7 @@ public class lab3Main {
                 .groupByKey()
                 .map(x -> {
                         Iterator<FlightInfoSer> flightInfo = x._2().iterator();
-                        Long cancelled = 0L, countCans = 0L, countDel = 0L, maxDelTime = 0L, delTime = 0L;
+                        Long countCancelled = 0L, count = 0L, countDel = 0L, maxDelTime = 0L, delTime = 0L;
                         while (flightInfo.hasNext()) {
                             FlightInfoSer nextInfo = flightInfo.next();
                             delTime = nextInfo.getDelayTime();
@@ -34,8 +40,18 @@ public class lab3Main {
                                 countDel++;
                             }
                             maxDelTime = max(maxDelTime, delTime);
+                            countCancelled += nextInfo.getIfCancelled();
+                            count++;
                         }
+                        return  new Tuple2<>(
+                                new Tuple2<>(
+                                        airportsBroadcasted.getValue().get(x._1()._1()),
+                                        airportsBroadcasted.getValue().get(x._1()._2())
+                                ),
+                                maxDelTime + " " + countDel*100/count + "% " + countCancelled*100/count + "%"
+                        );
                 })
+                .saveAsTextFile("lab3_output");
 
     }
 
