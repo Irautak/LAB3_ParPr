cd9d021901d1f2fab103feb3f04486ce3daedac3
Vlad Lomtev <vladlomtev@gmail.com>
2020-12-04 18:58:30 +0300

    Scripted auto-commit on change (2020-12-04 18:58:30) by gitwatch.sh

--- src/main/java/lab3Main.java
+++ src/main/java/lab3Main.java
@@ -1,11 +1,12 @@
-import org.apache.spark
-
-
+import org.apache.spark.SparkConf;
+import org.apache.spark.api.java.JavaSparkContext;
 
 public class lab3Main {
     public static void main(String[] args) {
         SparkConf conf = new SparkConf().setAppName("lab3");
-        JavaSparkContent sc = new JavaSparkContext(conf);
+        JavaSparkContext sc = new JavaSparkContext(conf);
+        val RDD = sc.textFile("file.csv");
+        val RDDPairs = 
 
     }
 
